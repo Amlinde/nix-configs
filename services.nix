84edb030ac33b3fb2cb0e1bcca787d83753ecc63
Alex Linde <amlinde@live.com>
2024-03-03 11:19:39 -0600

    SDDM Enabled Numlock

--- services.nix
+++ services.nix
@@ -7,7 +7,10 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.displayManager.sddm = {
+    enable = true;
+    autoNumlock = true;
+  };
   services.xserver.desktopManager.plasma5.enable = true;
 
   # Configure keymap in X11
