61eab01204beace47d1a5db21a6e8ad0077dab8f
Alex Linde <amlinde@live.com>
2024-03-05 16:04:11 -0600

    Boilerplate

--- pkgs/pkgs-laptop.nix
+++ pkgs/pkgs-laptop.nix
@@ -1,2 +1,4 @@
 { pkgs, ... }:
-{ }
+{
+  environment.systemPackages = with pkgs; [ ];
+}
--- pkgs/pkgs-pc.nix
+++ pkgs/pkgs-pc.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  environment.systemPackages = [
+  environment.systemPackages = with pkgs; [
     legendary-gl
   ];
   programs.steam.enable = true;
