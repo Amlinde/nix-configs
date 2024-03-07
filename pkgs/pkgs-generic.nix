163734ea43689f42ce068d1105fed333d2c1b5ef
Alex Linde <amlinde@live.com>
2024-03-07 11:42:40 -0600

    Added Rider

--- pkgs/pkgs-generic.nix
+++ pkgs/pkgs-generic.nix
@@ -1,5 +1,8 @@
 { pkgs, ... }:
 {
+  imports = [
+    ./rider.nix
+  ];
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
