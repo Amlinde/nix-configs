18474cab61611ef88751ac1b893564cb40448741
Alex Linde <amlinde@live.com>
2024-04-18 23:23:17 -0500

    Fixed Gamescope

--- pkgs/pkgs-pc.nix
+++ pkgs/pkgs-pc.nix
@@ -4,9 +4,24 @@
     legendary-gl
     ckb-next
     gamescope
-    mangohud
   ];
 
   programs.gamemode.enable = true;
+  nixpkgs.config.packageOverrides = pkgs: {
+    steam = pkgs.steam.override {
+      extraPkgs = pkgs: with pkgs; [
+        xorg.libXcursor
+        xorg.libXi
+        xorg.libXinerama
+        xorg.libXScrnSaver
+        libpng
+        libpulseaudio
+        libvorbis
+        stdenv.cc.cc.lib
+        libkrb5
+        keyutils
+      ];
+    };
+  };
   programs.steam.enable = true;
 }
