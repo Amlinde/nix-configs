ebf5dd7b64dd341ddf0f4dc018b2093746bfa1bd
Alex Linde <amlinde@live.com>
2024-04-18 23:24:21 -0500

    Switched to UUIDs

--- system/fs-pc.nix
+++ system/fs-pc.nix
@@ -1,17 +1,17 @@
 { ... }:
 {
   fileSystems."/mnt/c" = {
-    device = "/dev/nvme0n1p2";
+    device = "/dev/disk/by-uuid/28A25E2CA25DFEAE";
     fsType = "ntfs-3g";
     options = [ "uid=1000" "gid=100" "rw" "user" "exec" "umask=000" ];
   };
   fileSystems."/mnt/d" = {
-    device = "/dev/sdb1";
+    device = "/dev/disk/by-uuid/C22EC3D52EC3C0A1";
     fsType = "ntfs-3g";
     options = [ "uid=1000" "gid=100" "rw" "user" "exec" "umask=000" ];
   };
   fileSystems."/mnt/e" = {
-    device = "/dev/nvme1n1p1";
+    device = "/dev/disk/by-uuid/F4EEEE03EEEDBDCC";
     fsType = "ntfs-3g";
     options = [ "uid=1000" "gid=100" "rw" "user" "exec" "umask=000" ];
   };
