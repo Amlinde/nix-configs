c8a363fb254fb6932008a137056ea195c2b9c006
Alex Linde <amlinde@live.com>
2024-08-17 07:56:32 -0500

    Sysrq

--- system/boot.nix
+++ system/boot.nix
@@ -3,4 +3,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.kernelModules = [ "amdgpu" ];
+
+  boot.kernel.sysctl."kernel.sysrq" = 176;
 }
